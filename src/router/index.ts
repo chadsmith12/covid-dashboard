ca812b2755d0e2c46ec6b09bcdf45708342dbfe2
Chad Smith <smith.chad12@gmail.com>
2020-04-20 23:50:10 -0500

    remove uneeded components

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,14 +11,6 @@ const routes = [
     component: Dashboard
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/country/:countryCode',
     name: 'CountryView',
     component: () => import(/* webpackChunkName: "countryView" */ '@/views/CountryView.vue'),
